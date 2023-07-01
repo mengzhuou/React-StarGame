8970a32b184a6872896bcbaee52e79c9fc710da2
mengzhuou <71995520+mengzhuou@users.noreply.github.com>
2023-07-01 11:28:11 -0400

    color and sum works

--- star-game/src/App.tsx
+++ star-game/src/App.tsx
@@ -3,12 +3,24 @@ import React from "react";
 
 interface PlayNumberProps {
   playNum: number;
+  numStatus: string;
 }
 
-const PlayNumber: React.FC<PlayNumberProps> = ({ playNum }) => {
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+const PlayNumber: React.FC<PlayNumberProps> = ({ playNum, numStatus }) => {
   return (
     <span>
-      <button className='number'>
+      <button 
+        className='number'
+        style={{ backgroundColor: colors[numStatus as keyof typeof colors] }}
+        onClick={()=>console.log("Num click", playNum)}
+      >
         {playNum}
       </button>
     </span>
@@ -22,8 +34,30 @@ class App extends React.Component<any,any> {
       starNum: this.getRandomNum(1, 9),
       clickNum: 9,
       availableNums: [1,2,3,4,5],
-      candidateNums: [2,3]
+      candidateNums: [2,3],
     }
+  }
+
+  numberStatus = (curNum: number) => {
+    const curAvaNum = this.state.availableNums;
+    const curCandNum = this.state.candidateNums;
+    if (!curAvaNum.includes(curNum)){
+      return 'used';
+    }
+    if (curCandNum.includes(curNum)){
+      return this.candidateWrong() ? 'wrong' : 'candidate';
+    }
+    return 'available'
+  }
+
+  candidateWrong = () => {
+    return this.sum(this.state.candidateNums) > this.state.starNum;
+  }
+
+  sum(arr: number[]): number{
+    //acc: accumulator, curr: curremt
+    //reduce method provide accumulated result
+    return arr.reduce((acc,curr) => acc+ curr, 0) 
   }
 
   starRange(min: number, max: number): number[] {
@@ -56,7 +90,11 @@ class App extends React.Component<any,any> {
             <div className='right'>
               <div className='grid'>
                 {this.starRange(1, clickNum).map((index)=>(
-                  <PlayNumber key={index} playNum={index}/>
+                  <PlayNumber 
+                    key={index} 
+                    playNum={index}
+                    numStatus={this.numberStatus(index)}
+                    />
                 ))}
               </div>
             </div>
