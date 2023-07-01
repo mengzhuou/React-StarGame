150f771c30c0feb2e5775a1aa594745f3fbe269f
mengzhuou <71995520+mengzhuou@users.noreply.github.com>
2023-06-30 21:00:38 -0400

    added basic framework, star and clickNum

--- star-game/src/App.tsx
+++ star-game/src/App.tsx
@@ -5,19 +5,48 @@ class App extends React.Component<any,any> {
   constructor(props:any){
     super(props);
     this.state = {
-      star: 9
+      starNum: 9,
+      clickNum: 9
     }
   }
+
+  starRange(min: number, max: number): number[] {
+    return Array.from({ length: max - min + 1 }, (_, i) => min + i);
+  }
+
   render() {
+    const { starNum, clickNum } = this.state;
     return (
       <div className="App">
         <div className="App-header">
-          <div className='left'>
-            {/* {Range(1,)} */}
-
+          <div className='gameBody'>
+            <div className='left'>
+              <div className='grid'>
+                {this.starRange(1, starNum).map((index)=>(
+                  <span
+                    key={index}
+                  >
+                    <div className='star'>
+                      &#9733;
+                    </div>
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className='right'>
+              <div className='grid'>
+                {this.starRange(1, clickNum).map((index)=>(
+                  <span
+                  key={index}
+                >
+                  <button className='number'>
+                    {index}
+                  </button>
+                </span>
+                ))}
+              </div>
+            </div>
           </div>
-          &#9733;
-
         </div>
     </div>
     );
